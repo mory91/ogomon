11a0908e4c3c3a63d71ca178310f678899460b9f
Morteza <mortezaho.1376@gmail.com>
2023-09-11 18:21:41 -0600

    Add send msg tracing

---
+++ python/sendmsg.py
@@ -0,0 +1,23 @@
+import argparse
+import sys
+from send import get_bpf, get_call_back
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--pid", default=None)
+args = parser.parse_args()
+
+if args.pid is None:
+    print("PID must set")
+    exit(0)
+
+bpf_obj = get_bpf(args.pid)
+bpf_obj.attach_kretprobe(event="__sys_sendto", fn_name="send_return")
+cb = get_call_back(bpf_obj)
+bpf_obj['events'].open_ring_buffer(cb)
+
+while True:
+    try:
+        bpf_obj.ring_buffer_consume()
+    except KeyboardInterrupt:
+        exit()
+    sys.stdout.flush()
