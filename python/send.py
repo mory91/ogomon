b7e32e56f98630522f56db11a0965deaed00aa03
Morteza <mortezaho.1376@gmail.com>
2023-09-12 13:04:45 -0600

    Sendto and SendMsg

--- python/send.py
+++ python/send.py
@@ -45,25 +45,3 @@ def get_call_back(bpf):
         event = bpf['events'].event(data)
         print("%d,%d" % (event.timestamp_ns + kstime, event.size))
     return callback
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--pid", default=None)
-args = parser.parse_args()
-
-if args.pid is None:
-    print("PID must set")
-    exit(0)
-
-
-bpf_obj = get_bpf(args.pid)
-bpf_obj.attach_kretprobe(event="__sys_sendmsg", fn_name="send_return")
-cb = get_call_back(bpf_obj)
-bpf_obj['events'].open_ring_buffer(cb)
-
-while True:
-    try:
-        bpf_obj.ring_buffer_consume()
-    except KeyboardInterrupt:
-        exit()
-    sys.stdout.flush()
