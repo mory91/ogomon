bef6b4c06541dce108c67362b6501c324b3de4de
Morteza <mortezaho.1376@gmail.com>
2023-10-17 00:45:39 -0600

    Add sendmsg

--- python/sendmsg_fd.py
+++ python/sendmsg_fd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
+import sys
 from send import get_bpf, get_call_back
 
 parser = argparse.ArgumentParser()
@@ -12,9 +12,8 @@ if args.pid is None:
     print("PID must set")
     exit(0)
 
-
 bpf_obj = get_bpf(args.pid)
-bpf_obj.attach_kprobe(event="tcp_sendmsg", fn_name="send_entry")
+bpf_obj.attach_kprobe(event="__sys_sendmsg", fn_name="send_entry")
 cb = get_call_back(bpf_obj)
 bpf_obj['events'].open_ring_buffer(cb)
 
